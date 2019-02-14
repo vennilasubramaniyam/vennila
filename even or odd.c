3763f3593105ca8b9c623832c1e55919eebbcf99
vennilasubramaniyam <47620752+vennilasubramaniyam@users.noreply.github.com>
2019-02-14 10:30:05 +0530

    Create even or odd.c

---
+++ even or odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("check the number is even or odd");
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is odd");
+    }
+return 0;
+}
