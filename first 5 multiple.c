abef31b0116f4d671b1f2e44d1f72717430524e2
vennilasubramaniyam <47620752+vennilasubramaniyam@users.noreply.github.com>
2019-08-13 03:30:27 -1200

    Create first 5 multiple.c

---
+++ first 5 multiple.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        int sum=0,i,n;
+        printf("\n Please Give The Value of N:  ");
+        scanf("%d",&n);
+        for(i=1;i<=5;i++)
+        {
+            printf("\n %d",n*i);
+        }
+        
+}
